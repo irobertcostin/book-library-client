9a1ad110f2ac00b505b9c7993a5c4360f7733e15
irobertcostin <irobertcostin@gmail.com>
2023-02-18 01:10:07 +0200

    some design changes

--- client/app.js
+++ client/app.js
@@ -1,11 +1,49 @@
-let main = document.querySelector(".main")
-main.innerHTML=navbar();
-main.innerHTML+=loginTab();
-
-main.appendChild(divForBooks());
-populateDivForBooks();
-
-main.appendChild(divForCourses());
-populateDivForCourses();
+let navibar = document.querySelector(".navbar-parent")
+navibar.innerHTML=loginTab();
 
 
+let menu = document.querySelector(".menu-div")
+menu.innerHTML=navbar();
+
+
+let studentsBtn = document.querySelector(".students-btn")
+let booksBtn = document.querySelector(".books-btn")
+let coursesBtn = document.querySelector(".courses-btn")
+let enrollsBtn = document.querySelector(".enrolls-btn")
+
+
+studentsBtn.addEventListener("click",(e)=>{
+
+    let obj=e.target;
+
+    console.log(obj)
+
+
+})
+
+booksBtn.addEventListener("click",(e)=>{
+
+    let obj=e.target;
+
+    console.log(obj)
+
+
+})
+
+coursesBtn.addEventListener("click",(e)=>{
+
+    let obj=e.target;
+
+    console.log(obj)
+
+
+})
+
+enrollsBtn.addEventListener("click",(e)=>{
+
+    let obj=e.target;
+
+    console.log(obj)
+
+
+})
