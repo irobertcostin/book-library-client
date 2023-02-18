a2557977e0f6f85922d2bff0f84ec98e316e9eb4
irobertcostin <irobertcostin@gmail.com>
2023-02-18 11:27:35 +0200

    design updates

--- client/app.js
+++ client/app.js
@@ -1,3 +1,6 @@
+let main = document.querySelector(".main");
+
+
 let navibar = document.querySelector(".navbar-parent")
 navibar.innerHTML=loginTab();
 
@@ -5,18 +8,36 @@ navibar.innerHTML=loginTab();
 let menu = document.querySelector(".menu-div")
 menu.innerHTML=navbar();
 
+let contentDiv = document.querySelector(".content-div")
+contentDiv.appendChild(marketing())
+
 
 let studentsBtn = document.querySelector(".students-btn")
 let booksBtn = document.querySelector(".books-btn")
 let coursesBtn = document.querySelector(".courses-btn")
 let enrollsBtn = document.querySelector(".enrolls-btn")
 
+main.addEventListener("click",(e)=>{
+
+    let obj = e.target
+    let landImg=document.querySelector(".land-img")
+   
+    
+    if(obj.classList.contains("logo-img")){
+        document.querySelector(".content-div").innerHTML="";
+        document.querySelector(".content-div").appendChild(marketing());
+    }
+})
+
 
 studentsBtn.addEventListener("click",(e)=>{
 
     let obj=e.target;
 
-    
+    let contentDiv = document.querySelector(".content-div");
+    contentDiv.innerHTML="";
+    contentDiv.appendChild(divForStudents());
+    populateDivForStudents()
 
 
 })
